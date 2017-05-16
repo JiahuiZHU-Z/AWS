8011d9daf3c5af41c9a393f89fed1db40ea93ad7
jiahui8Zhu <jiahui.zhu1503@gmail.com>
2017-05-16 17:02:01 +0200

    2nd commit

--- app.js
+++ app.js
@@ -1,24 +1,39 @@
+var AWS = require('aws-sdk');
 var express = require('express'), http = require('http');
 var app = express();
 
 
-
-var mysql = require('mysql');
-
+AWS.config.update({
+	region: process.env.DYNAMO_REGION,
+	endpoint: process.env.DYNAMO_ENDPOINT
+});
 
 app.get('/', function (req, res) {
-    res.send('Wow');
+	res.send('Wowghjwgowhifowdo');
 });
 
-app.get('/users', function (req, res) {
-    res.send('Wow');
-});
-
-app.get('/user/:userid', function (req, res) {
-	var docClient = new AWS.
+app.get('/students', function (req, res) {
 	
+
 });
+
+app.get('/user/:userId', function(req, res) {
+	var docClient = new AWS.DynamoDB.DocumentClient();
+	var params = {
+		TableName: "users",
+		Key: {
+			"userId"; req.params["userId"];
+		}
+	}
+	docClient.get(params, function(err,data) {
+		if(err) {
+			res.send("Error while reading: " + JSON.stringify(err, nulln2));
+		} else {
+			res.send("Get item: " + JSON.stringify(data, null, 2));
+		}
+	});
+})
 
 app.listen(process.env.PORT, function () {
-    console.log("Eventually server successfully started!");
+	console.log("server successfully started!");
 });
